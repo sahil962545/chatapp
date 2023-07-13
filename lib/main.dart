1d86a2ef744de78516025be65124ace17950fe11
unknown <sahillohiya962545@gmail.com>
2023-07-13 15:21:47 +0530

    edit main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String title
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
